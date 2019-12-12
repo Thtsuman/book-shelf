f7626df86a2458d9d3a23d3adda74610352b25e3
Thtsuman <tht.suman@gmail.com>
2019-12-12 21:54:09 +0530

    Updates

---
+++ precache-manifest.89c368955e71585bfcd5ad0728aa125b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cc44c91b82278c2910ac453d18fc67",
+    "url": "/book-shelf/index.html"
+  },
+  {
+    "revision": "47b69fb826bbfe184671",
+    "url": "/book-shelf/static/js/2.edf68913.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/book-shelf/static/js/2.edf68913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9bf046f7c49a016aad4",
+    "url": "/book-shelf/static/js/main.29b6df89.chunk.js"
+  },
+  {
+    "revision": "fdc1c09c3b9d044559ef",
+    "url": "/book-shelf/static/js/runtime-main.887f0577.js"
+  }
+]);
